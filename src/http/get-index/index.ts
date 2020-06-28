43eafbdf84b5361b734b02172e0f9822494fcc84
FeStack <40145146+festackcode@users.noreply.github.com>
2020-06-28 16:16:11 +0200

    Update index.ts
    kick port 5000

--- src/http/get-index/index.ts
+++ src/http/get-index/index.ts
@@ -1,22 +1,12 @@
 import { serve } from "https://deno.land/std@0.54.0/http/server.ts";
-import { Application } from "https://deno.land/x/oak/mod.ts";
-import { Users } from "./users.model.ts";
-import router from "./routes.ts";
+// import { Application } from "https://deno.land/x/oak/mod.ts";
+// import { Users } from "./users.model.ts";
+// import router from "./routes.ts";
 
 // const s = serve({ port: 8000 });
 const port = 5000;
-// console.log("http://localhost:8000/");
-// for await (const req of s) {
-//   req.respond({ body: "Hello World\n" });
-// }
+console.log("http://localhost:5000/");
+for await (const req of s) {
+  req.respond({ body: "Hello World\n" });
+}
 
-// init app
-const app = new Application();
-
-// api routes/methods
-app.use(router.routes());
-app.use(router.allowedMethods());
-
-console.log(`Server running on port ${port}`);
-
-await app.listen ({port})
