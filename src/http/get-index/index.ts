aa2869ca645f693b8db6ea312d010219da855e60
FeStack <40145146+festackcode@users.noreply.github.com>
2020-06-28 16:21:58 +0200

    Update index.ts

--- src/http/get-index/index.ts
+++ src/http/get-index/index.ts
@@ -1,12 +1,53 @@
-import { serve } from "https://deno.land/std@0.54.0/http/server.ts";
-// import { Application } from "https://deno.land/x/oak/mod.ts";
-// import { Users } from "./users.model.ts";
-// import router from "./routes.ts";
-
-// const s = serve({ port: 8000 });
-const port = 5000;
-console.log("http://localhost:5000/");
-for await (const req of s) {
-  req.respond({ body: "Hello World\n" });
+/ TODO: modify the body object!
+let body = {
+message: "hello world"
 }
 
+export async function handler (req: object) {
+  return {
+    headers: {
+      'content-type': 'application/json; charset=utf8',
+      'cache-control': 'no-cache, no-store, must-revalidate, max-age=0, s-maxage=0'
+    },
+    body: JSON.stringify(body)
+  }
+}
+
+// Example responses
+
+/* Forward requester to a new path
+export async function handler (req: object) {
+  return {
+    statusCode: 302,
+    headers: {
+      'location': '/about',
+      'cache-control': 'no-cache, no-store, must-revalidate, max-age=0, s-maxage=0'
+    }
+  }
+}
+*/
+
+/* Respond with successful resource creation
+export async function handler (req: object) {
+  return {
+    statusCode: 201,
+    headers: {
+      'content-type': 'application/json; charset=utf8',
+      'cache-control': 'no-cache, no-store, must-revalidate, max-age=0, s-maxage=0'
+    },
+    body: JSON.stringify({ok: true})
+  }
+}
+*/
+
+/* Deliver client-side JS
+export async function handler (req: object) {
+  return {
+    headers: {
+      'content-type': 'text/javascript; charset=utf8',
+      'cache-control': 'no-cache, no-store, must-revalidate, max-age=0, s-maxage=0'
+    },
+    body: 'console.log("Hello world!")',
+  }
+}
+*/
